d0c6454e75be802f0733cb26617eb27220d8bf53
Abroor Rizky <abroorrizky@gmail.com>
2025-03-26 23:10:08 +0700

    Update Dashboard View

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BelajarController;
 use App\Http\Controllers\LatihanController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,8 +22,14 @@ Route::post('action-tambah', [BelajarController::class, 'actionTambah']);
 Route::get('latihan', [LatihanController::class, 'index']);
 
 //LOGIN
-Route::get('login', [LoginController::class, 'login']);
+// Route::get('login', [LoginController::class, 'login']);
+Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('action-login', [LoginController::class, 'actionLogin']);
 
 //DASHBOARD
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+// Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth');
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
